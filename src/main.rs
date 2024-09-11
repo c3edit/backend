fdf92a574a449f71d59dad685ef11780e4fdba9d
Adam Zhang <898544@lcps.org>
2024-09-11 11:51:35 -0400

    feat: working tcp connection
    This is a mess of unrelated test code right now. Time to merge it into something cohesive.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
+use std::{io::{self, Read, Write}, net::{TcpListener, TcpStream}};
+
 use crdts::{list::Op, CmRDT, List};
 
 fn main() {
+    let mut stream: TcpStream;
+    let args = std::env::args().collect::<Vec<String>>();
+    let server = args.contains(&String::from("server"));
+    if server {
+        let listener = TcpListener::bind("0.0.0.0:6969");
+        stream = listener.unwrap().accept().unwrap().0;
+    } else {
+        let mut address = String::new();
+        io::stdin().read_line(&mut address).unwrap();
+        stream = TcpStream::connect(address.trim()).unwrap();
+    }
+
+    if server {
+        let mut s = String::new();
+        stream.read_to_string(&mut s).unwrap();
+        println!("{:?}", s);
+    } else {
+        write!(stream, "Hello world").unwrap();
+    }
+    
     let mut text = List::new();
 
     "Hello world".chars().for_each(|c| {

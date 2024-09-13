8208ff851fb8f4dcd0adb9137d45eed1dba9a017
Adam Zhang <898544@lcps.org>
2024-09-13 15:34:20 -0400

    feat: basic changes sync
    For some reason the CRDT isn't converging to sahe same state, so I'm just going
    to swap out the library ¯\_(ツ)_/¯.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,141 @@
-use std::{io::{self, Read, Write}, net::{TcpListener, TcpStream}};
-
 use crdts::{list::Op, CmRDT, List};
+use futures::{SinkExt, TryStreamExt};
+use std::{
+    io::{self},
+    sync::Arc,
+};
+use tokio::{
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpListener, TcpStream,
+    },
+    sync::Mutex,
+};
+use tokio_serde::formats::SymmetricalJson;
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 
-fn main() {
-    let mut stream: TcpStream;
+#[tokio::main]
+async fn main() {
+    let stream: TcpStream;
+
     let args = std::env::args().collect::<Vec<String>>();
     let server = args.contains(&String::from("server"));
     if server {
-        let listener = TcpListener::bind("0.0.0.0:6969");
-        stream = listener.unwrap().accept().unwrap().0;
+        let listener = TcpListener::bind("0.0.0.0:6969").await.unwrap();
+        stream = listener.accept().await.unwrap().0;
     } else {
         let mut address = String::new();
         io::stdin().read_line(&mut address).unwrap();
-        stream = TcpStream::connect(address.trim()).unwrap();
+        stream = TcpStream::connect(address.trim()).await.unwrap();
     }
+
+    let mut text = Text::new(stream);
+
+    text.begin_update_task();
 
     if server {
-        let mut s = String::new();
-        stream.read_to_string(&mut s).unwrap();
-        println!("{:?}", s);
+        text.append_string(String::from("Hello world")).await;
     } else {
-        write!(stream, "Hello world").unwrap();
+        text.append_string(String::from("Foobar")).await;
     }
-    
-    let mut text = List::new();
 
-    "Hello world".chars().for_each(|c| {
-        text.apply(text.append(c, 1))
-    });
+    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+    println!("After update: {:?}", text.read().await);
 
-    (6..11).rev().for_each(|i| {
-        text.apply(text.delete_index(i, 1).unwrap())
-    });
-    "mom".chars().for_each(|c| text.apply(text.append(c, 1)));
+    // if server {
+    //     let mut s = String::new();
+    //     stream.read_to_string(&mut s).unwrap();
+    //     println!("{:?}", s);
+    // } else {
+    //     write!(stream, "Hello world").unwrap();
+    // }
+    //
+    // "Hello world"
+    //     .chars()
+    //     .for_each(|c| text.apply(text.append(c, 1)));
 
-    text.apply(text.delete_index(4, 2).unwrap());
+    //     (6..11)
+    //     .rev()
+    //     .for_each(|i| text.apply(text.delete_index(i, 1).unwrap()));
+    // "mom".chars().for_each(|c| text.apply(text.append(c, 1)));
+    //
+    // text.apply(text.delete_index(4, 2).unwrap());
+    //
+    // let s: String = text.read();
+    // println!("{:?}", s);
+}
 
-    let s: String = text.read();
-    println!("{:?}", s);
+struct Text {
+    data: Arc<Mutex<List<char, u32>>>,
+    // I hate Rust sometimes.
+    write_socket: tokio_serde::SymmetricallyFramed<
+        FramedWrite<OwnedWriteHalf, LengthDelimitedCodec>,
+        Op<char, u32>,
+        SymmetricalJson<Op<char, u32>>,
+    >,
+    read_socket: Option<
+        tokio_serde::SymmetricallyFramed<
+            FramedRead<OwnedReadHalf, LengthDelimitedCodec>,
+            Op<char, u32>,
+            SymmetricalJson<Op<char, u32>>,
+        >,
+    >,
+}
+
+impl Text {
+    fn new(socket: TcpStream) -> Self {
+        socket.set_nodelay(true).unwrap();
+        let (read, write) = socket.into_split();
+        let read_framed = tokio_serde::SymmetricallyFramed::new(
+            FramedRead::new(read, LengthDelimitedCodec::new()),
+            SymmetricalJson::<Op<char, u32>>::default(),
+        );
+        let write_framed = tokio_serde::SymmetricallyFramed::new(
+            FramedWrite::new(write, LengthDelimitedCodec::new()),
+            SymmetricalJson::<Op<char, u32>>::default(),
+        );
+        Text {
+            data: Arc::new(Mutex::new(List::new())),
+            write_socket: write_framed,
+            read_socket: Some(read_framed),
+        }
+    }
+
+    async fn apply_op(&mut self, op: Op<char, u32>) {
+        self.data.lock().await.apply(op);
+    }
+
+    async fn broadcast_op(&mut self, op: Op<char, u32>) {
+        self.write_socket.send(op).await.unwrap();
+    }
+
+    async fn append_string(&mut self, s: String) {
+        let mut ops: Vec<Op<_, _>> = Vec::new();
+
+        for c in s.chars() {
+            let op = self.data.lock().await.append(c, 1);
+            self.apply_op(op.clone()).await;
+            ops.push(op);
+        }
+
+        for op in ops {
+            self.broadcast_op(op).await;
+        }
+    }
+
+    fn begin_update_task(&mut self) {
+        let mut socket = self.read_socket.take().unwrap();
+        let data = self.data.clone();
+
+        tokio::spawn(async move {
+            while let Some(op) = socket.try_next().await.unwrap() {
+                println!("Received op: {:?}", op);
+                data.lock().await.apply(op);
+            }
+        });
+    }
+
+    async fn read(&self) -> String {
+        self.data.lock().await.iter().collect()
+    }
 }

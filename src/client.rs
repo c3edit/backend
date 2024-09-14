6e9b665fc9311dcd9f85f4a271881a2906be44a2
Adam Zhang <898544@lcps.org>
2024-09-14 09:05:06 -0400

    refactor: move structs to client module

---
+++ src/client.rs
@@ -0,0 +1,85 @@
+use futures::{SinkExt, TryStreamExt};
+use loro::LoroDoc;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::{
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpStream,
+    },
+    sync::Mutex,
+};
+use tokio_serde::formats::SymmetricalJson;
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Message {
+    data: Vec<u8>,
+}
+
+pub struct Client {
+    doc: Arc<Mutex<LoroDoc>>,
+    // I hate Rust sometimes.
+    write_socket: tokio_serde::SymmetricallyFramed<
+        FramedWrite<OwnedWriteHalf, LengthDelimitedCodec>,
+        Message,
+        SymmetricalJson<Message>,
+    >,
+    read_socket: Option<
+        tokio_serde::SymmetricallyFramed<
+            FramedRead<OwnedReadHalf, LengthDelimitedCodec>,
+            Message,
+            SymmetricalJson<Message>,
+        >,
+    >,
+}
+
+impl Client {
+    pub fn new(socket: TcpStream) -> Self {
+        socket.set_nodelay(true).unwrap();
+
+        let (read, write) = socket.into_split();
+        let read_framed = tokio_serde::SymmetricallyFramed::new(
+            FramedRead::new(read, LengthDelimitedCodec::new()),
+            SymmetricalJson::<Message>::default(),
+        );
+        let write_framed = tokio_serde::SymmetricallyFramed::new(
+            FramedWrite::new(write, LengthDelimitedCodec::new()),
+            SymmetricalJson::<Message>::default(),
+        );
+
+        Client {
+            doc: Arc::new(Mutex::new(LoroDoc::new())),
+            write_socket: write_framed,
+            read_socket: Some(read_framed),
+        }
+    }
+
+    pub async fn broadcast_changes(&mut self) {
+        let message = Message {
+            data: self.doc.lock().await.export_from(&Default::default()),
+        };
+        self.write_socket.send(message).await.unwrap();
+    }
+
+    pub async fn append_string(&mut self, s: &str) {
+        let doc = self.doc.lock().await;
+        doc.get_text("text").insert(0, s).unwrap();
+    }
+
+    pub fn begin_update_task(&mut self) {
+        let mut socket = self.read_socket.take().unwrap();
+        let data = self.doc.clone();
+
+        tokio::spawn(async move {
+            while let Some(message) = socket.try_next().await.unwrap() {
+                println!("Received message: {:?}", message);
+                data.lock().await.import(&message.data).unwrap();
+            }
+        });
+    }
+
+    pub async fn read(&self) -> String {
+        self.doc.lock().await.get_text("text").to_string()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use futures::{SinkExt, TryStreamExt};
-use loro::LoroDoc;
-use serde::{Deserialize, Serialize};
-use std::sync::Arc;
-use tokio::{
-    net::{
-        tcp::{OwnedReadHalf, OwnedWriteHalf},
-        TcpListener, TcpStream,
-    },
-    sync::Mutex,
-};
-use tokio_serde::formats::SymmetricalJson;
-use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+mod client;
+
+use client::Client;
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() {
@@ -25,7 +16,7 @@ async fn main() {
         stream = TcpStream::connect("localhost:6969").await.unwrap();
     }
 
-    let mut text = Text::new(stream);
+    let mut text = Client::new(stream);
 
     text.begin_update_task();
 
@@ -38,74 +29,4 @@ async fn main() {
     text.broadcast_changes().await;
     tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
     println!("After update: {:?}", text.read().await);
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-struct Message {
-    data: Vec<u8>,
-}
-
-struct Text {
-    doc: Arc<Mutex<LoroDoc>>,
-    // I hate Rust sometimes.
-    write_socket: tokio_serde::SymmetricallyFramed<
-        FramedWrite<OwnedWriteHalf, LengthDelimitedCodec>,
-        Message,
-        SymmetricalJson<Message>,
-    >,
-    read_socket: Option<
-        tokio_serde::SymmetricallyFramed<
-            FramedRead<OwnedReadHalf, LengthDelimitedCodec>,
-            Message,
-            SymmetricalJson<Message>,
-        >,
-    >,
-}
-
-impl Text {
-    fn new(socket: TcpStream) -> Self {
-        socket.set_nodelay(true).unwrap();
-        let (read, write) = socket.into_split();
-        let read_framed = tokio_serde::SymmetricallyFramed::new(
-            FramedRead::new(read, LengthDelimitedCodec::new()),
-            SymmetricalJson::<Message>::default(),
-        );
-        let write_framed = tokio_serde::SymmetricallyFramed::new(
-            FramedWrite::new(write, LengthDelimitedCodec::new()),
-            SymmetricalJson::<Message>::default(),
-        );
-        Text {
-            doc: Arc::new(Mutex::new(LoroDoc::new())),
-            write_socket: write_framed,
-            read_socket: Some(read_framed),
-        }
-    }
-
-    async fn broadcast_changes(&mut self) {
-        let message = Message {
-            data: self.doc.lock().await.export_from(&Default::default()),
-        };
-        self.write_socket.send(message).await.unwrap();
-    }
-
-    async fn append_string(&mut self, s: &str) {
-        let doc = self.doc.lock().await;
-        doc.get_text("text").insert(0, s).unwrap();
-    }
-
-    fn begin_update_task(&mut self) {
-        let mut socket = self.read_socket.take().unwrap();
-        let data = self.doc.clone();
-
-        tokio::spawn(async move {
-            while let Some(message) = socket.try_next().await.unwrap() {
-                println!("Received message: {:?}", message);
-                data.lock().await.import(&message.data).unwrap();
-            }
-        });
-    }
-
-    async fn read(&self) -> String {
-        self.doc.lock().await.get_text("text").to_string()
-    }
 }

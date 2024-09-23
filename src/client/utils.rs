07a5c61c2d7600cafcadbb012f021e782359cd47
Adam Zhang <898544@lcps.org>
2024-09-23 18:22:49 -0400

    fix(client/utils): use dash when generating unique name

--- src/client/utils.rs
+++ src/client/utils.rs
@@ -6,7 +6,7 @@ pub fn generate_unique_id(name: &str, doc: &mut LoroDoc) -> String {
 
     while !doc.get_text(unique_name.as_str()).is_empty() {
         i += 1;
-        unique_name = format!("{} {}", name, i);
+        unique_name = format!("{}-{}", name, i);
     }
 
     unique_name
